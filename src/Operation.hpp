8ed751e6460774799c87099e3d3adae38fc51cb0
yair koskas <yairi2003@gmail.com>
2020-08-28 19:25:57 +0300

    modified Operation.hpp spacing

--- src/Operation.hpp
+++ src/Operation.hpp
@@ -3,13 +3,10 @@
 #include <vector>
 
 class Operation {
-std::vector<std::string> m_input;
-std::string m_output;
-
-
-
-Operation(const std::vector<std::string>& input);
-
-std::string doInput();
-
+    private:
+        std::vector<std::string> m_input;
+        std::string m_output;
+    public:
+        Operation(const std::vector<std::string>& input);
+        std::string doInput();
 };
