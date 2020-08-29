90c762a33ab80fb766f43a78826f4800630d276e
yair koskas <yairi2003@gmail.com>
2020-08-29 16:07:10 +0300

    added necessary include lines

--- src/FileToObject.cpp
+++ src/FileToObject.cpp
@@ -1,10 +1,12 @@
-#include "FileToObject"
+#include "FileToObject.hpp"
+#include "BMP.h"
+#include "CMatrix.h"
 FileToObject::FileToObject(std::string type, std::string pathfile1, std::string pathfile2) {
     if (type == "matrix") {
         
     }
     if (type == "image") {
-        this->image = Image(pathfile1);
+        this->image = BMP(pathfile1);
     }
     if (type == "bitfile") {
         bitfile = pathfile1;
--- src/FileToObject.hpp
+++ src/FileToObject.hpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string>
+#include "CMatrix.h"
+#include "BMP.h"
 class FileToObject {
     private:
-        Matrix matrix1;
-        Matrix matrix2;
-        Image image;
+        CMatrix matrix1;
+        CMatrix matrix2;
+        BMP image;
         crc32 bitfile;
 
     public:
         FileToObject (std::string type, std::string pathfile1, std::string pathfile2);
-};
+};s
