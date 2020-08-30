80d9841463b51b657989f4ccff6c692b38872253
Shaked Winder <aesh2003@gmail.com>
2020-08-30 17:08:18 +0300

    add constructor of matrix from text file

--- src/FileToObject.cpp
+++ src/FileToObject.cpp
@@ -1,14 +1,82 @@
 #include "FileToObject.hpp"
 #include "BMP.h"
 #include "CMatrix.h"
-FileToObject::FileToObject(std::string type, std::string pathfile1, std::string pathfile2) {
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+FileToObject::FileToObject(std::string type, std::string pathfile1, std::string pathfile2,
+const std::string& outputPath1) {
+    this->outputPath = outputPath1;
     if (type == "matrix") {
-        
+        std::ifstream matrix;
+        std::string line;
+
+        matrix.open (pathfile1);
+        std::vector<std::vector<double>> mat;
+        while(getline(matrix, line)) {
+            std::vector<double> vec;
+            for (int i = 0; i < line.length(); i++) {
+                if (line[i] != ',') {
+                    char* c = &line[i];
+                    double num = atof(c);
+                    vec.push_back(num);
+                }
+            }
+            mat.push_back(vec);
+        };
+        matrix.close();
+        this->matrix1 = CMatrix(mat.size(), mat[0].size());
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[0].size(); j++){
+                this->matrix1.setValue(i, j, mat[i][j]);
+            }
+        }
+
+        matrix.open (pathfile2);
+        std::vector<std::vector<double>> mat2;
+        while(getline(matrix, line)) {
+            std::vector<double> vec;
+            for (int i = 0; i < line.length(); i++) {
+                if (line[i] != ',') {
+                    char* c = &line[i];
+                    double num = atof(c);
+                    vec.push_back(num);
+                }
+            }
+            mat.push_back(vec);
+        };
+        matrix.close(); 
+        this->matrix2 = CMatrix(mat2.size(), mat2[0].size());
+        for (int i = 0; i < mat2.size(); i++) {
+            for (int j = 0; j < mat2[0].size(); j++){
+                this->matrix2.setValue(i, j, mat2[i][j]);
+            }
+        }
+
     }
     if (type == "image") {
-        this->image = BMP(pathfile1);
+        this->image = pathfile1;
     }
     if (type == "bitfile") {
-        bitfile = pathfile1;
+        //bitfile = pathfile1;
     }
+
+}
+
+CMatrix FileToObject :: addMatrix() {
+    CMatrix mat = this->matrix1.add(this->matrix1, this->matrix2);
+    return mat;
+}
+CMatrix FileToObject :: multiplyMatrix() {
+    CMatrix mat = this->matrix1.multMatrix(this->matrix1, this->matrix2);
+    return mat;
+}
+
+void FileToObject :: rotateBmp() {
+    rotate_image(this->image, this->outputPath);
+
+}
+void FileToObject :: grayBmp() {
+    convert_to_grayscale(this->image, this->outputPath);
 }
--- src/FileToObject.hpp
+++ src/FileToObject.hpp
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <string>
-#include "CMatrix.h"
+#include "matrix/CMatrix.h"
 #include "BMP.h"
+#include "bmp_tester.hpp"
 class FileToObject {
     private:
         CMatrix matrix1;
         CMatrix matrix2;
-        BMP image;
-        crc32 bitfile;
+        std::string image;
+        //crc32 bitfile;
+        std::string outputPath;
+
 
     public:
-        FileToObject (std::string type, std::string pathfile1, std::string pathfile2);
-};s
+        FileToObject (std::string type, std::string pathfile1, std::string pathfile2,
+         const std::string& outputPath1);
+
+        CMatrix addMatrix();
+        CMatrix multiplyMatrix();
+
+        void rotateBmp();
+        void grayBmp();
+};
