a53f7552fc69834dbfd0925786650e0916a7773f
yair koskas <yairi2003@gmail.com>
2020-09-03 18:55:18 +0300

    implemented clear

--- src/cacheManager.cpp
+++ src/cacheManager.cpp
@@ -1,5 +1,12 @@
 #include "cacheManager.hpp"
+
 #include <iostream>
+#include <filesystem>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fstream>
+#include <experimental/filesystem>
+#include <unistd.h>
 
 cacheManager::cacheManager(std::string command) {
     this->m_command = command;
@@ -10,55 +17,18 @@ void cacheManager::doCommand() {
     std::string com = this->m_command;
     int i = 0;
     int j = 0;
-    while (i < com.size()) {
-        while (com[i] != ' ') {
-            std::string str;
-            str = str + com[i]
+    while (!com.empty()) {
+        if (com[i] == ' ') {
             i++;
+            j++;
         }
-        command.push_back(str);
-        j++;
+        command[j].push_back(com[i]);
     }
     Operation operation = Operation(command);
-    std::fstream cache;
-    std::string line;
-    std::string order;
-    for (int n = 0; n < command.size - 2; n++) {
-        order = order + command[n];
-    } 
 
-    cache.open("cache/manage.txt");
-    int numline = 0;
-    while(getline(cache, line)) {
-        string::size_type loc = line.find(order, 0);
-        if (loc != string::npos) {
-            std::fstream end;
-            end.open(command[command.size - 1]);
-            std::fstream found;
+    operation.doInput();
+}
 
-            std::vector<std::string> lin;
-            i = 0;
-            j = 0;
-            while (i < line.size()) {
-                while (line[i] != ' ') {
-                    std::string str;
-                    str = str + line[i]
-                    i++;
-                    }
-                    lin.push_back(str);
-                    j++;
-                    }
-
-            found.open(lin[lin.size() - 1]);
-            copy(found, end);
-
-            end.close();
-            found.close();
-            std::cout << "found solution in cache" << std::endl;
-            return;
-        }         
-        numline++;
-    }
-       cache << operation.doInput();
-       cache << "\n";
+void cacheManager::clear() {
+   std::experimental::filesystem::remove_all("./bin/cache");
 }
