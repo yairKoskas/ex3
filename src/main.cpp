ab734be79569bd4b57c6efd6840e1c47c9059c97
yair koskas <yairi2003@gmail.com>
2020-09-03 22:21:16 +0300

    changed cm constructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "cacheManager.hpp"
-int main(int argc, char** argv) {
-    cacheManager cm = cacheManager(std::string(*argv));
+int main(int argc , char** argv) {
+    cacheManager cm = cacheManager(argc,std::string(*argv));
     cm.doCommand();
     return 0;
 }
