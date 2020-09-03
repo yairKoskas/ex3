af52a51f2fadc069e45eb73554533115237c81fe
Shaked Winder <aesh2003@gmail.com>
2020-09-03 19:26:55 +0300

    add file imple

--- src/cacheManager.cpp
+++ src/cacheManager.cpp
@@ -16,17 +16,55 @@ void cacheManager::doCommand() {
     std::vector<std::string> command;
     std::string com = this->m_command;
     int i = 0;
-    int j = 0;
-    while (!com.empty()) {
-        if (com[i] == ' ') {
-            i++;
-            j++;
+     while (i < com.size()) {
+        while (com[i] != ' ') {
+            std::string str;
+            str = str + com[i]
+            i++
         }
-        command[j].push_back(com[i]);
-    }
+        i++;
+        command.push_back(str);
+     }
+    
     Operation operation = Operation(command);
 
-    operation.doInput();
+    std::fstream cache;
+    std::string line;
+    std::string order;
+    for (int n = 0; n < command.size - 2; n++) {
+        order = order + command[n];
+    } 
+
+    cache.open("cache/manage.txt");
+    int numline = 0;
+    while(getline(cache, line)) {
+        string::size_type loc = line.find(order, 0);
+        if (loc != string::npos) {
+            std::fstream end;
+            end.open(command[command.size - 1]);
+            std::fstream found;
+
+            std::vector<std::string> lin;
+            i = 0;
+            while (i < line.size()) {
+                while (line[i] != ' ') {
+                    std::string str;
+                    str = str + line[i]
+                    i++;
+                    }
+                    lin.push_back(str);
+                    }
+        found.open(lin[lin.size() - 1]);
+        copy(found, end);
+         end.close();
+        found.close();
+        std::cout << "found solution in cache" << std::endl;
+        return;
+        }
+    }
+    cache << operation.doInput();
+    cache << "\n";
+    cache.close();
 }
 
 void cacheManager::clear() {
